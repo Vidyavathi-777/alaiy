943355a68c433a2e1fd123b9f6d16976ae15c6b0
Vidyavathi777 <vidyavathidigadari_jtbb2@gmail.com>
2025-05-19 18:26:20 +0530

    first commit

---
+++ react/staticpage/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import './App.css'
+import Maincomponent from './main-component'
+
+function App() {
+
+  return (
+    <>
+    <Maincomponent />
+
+    </>
+  )
+}
+
+export default App
---
+++ react/staticpage/src/main-component.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from "/home/jtd/Documents/react/project-1/src/assets/react.svg"
+
+const Maincomponent = () => {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <img src={logo} alt="" width="50px" className='logo' />
+                    <h2 className='title'>ReactFacts</h2>
+                </nav>
+            </header>
+            <main>
+
+                <h1 className='body-title'>Fun facts about React</h1>
+                <ul className='list-div'>
+                    <li>was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100k stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thuand of enterprise apps,including mobile apps</li>
+                </ul>
+            </main>
+        </div>
+    )
+}
+
+export default Maincomponent
