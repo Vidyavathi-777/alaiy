9be4b5a8c65a7aa79296f6f0c96e49ac4d22c6c2
Vidyavathi777 <vidyavathidigadari_jtbb2@gmail.com>
2025-05-20 19:37:42 +0530

    meme generator

---
+++ meme-generator/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header className='header'>
+        <img src='https://chargeraccount.org/wp-content/uploads/2023/06/K7Kk3DSxN0r4AVhONa9ruggbas3DWRIrbnHw1dJc-1-900x600.jpg' className='logo'/>
+        <h1>Meme Generator</h1>
+        {/* <h4>React practice-project-4</h4> */}
+      </header>
+    </div>
+  )
+}
+
+export default Header
---
+++ meme-generator/src/Meme.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react'
+
+const Meme = () => {
+    const [meme, setMeme] = useState({
+        topText : "One does not simply",
+        bottomText : "Walk into mordor",
+        randomImage : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-l_EZ3g82s-Qxlz65lEaUEJwhnA05gOtRqg&s"
+    })
+
+    const [allMemes,setAllMemes] = useState([])
+
+    useEffect(() =>{
+        fetch(" https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    },[])
+
+    // console.log(allMemes)
+
+    function handleChange(event) {
+        const { value, name } = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
+    function getMemeImage (){
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme =>({
+            ...prevMeme,
+            randomImage:url
+        }))
+    }
+
+    return (
+        <main>
+            <div className='form'>
+                <label>
+                    Top Text</label>
+                    <input
+                        type='text'
+                        className='form-input'
+                        name='topText'
+                        placeholder='one does not simply '
+                        value={meme.topText}
+                        onChange={handleChange}
+                    />
+                
+                <label>
+                    Bottom Text </label>
+                    <input
+                        type='text'
+                        className='form-input'
+                        name='bottomText'
+                        placeholder='walk into mordor'
+                        value={meme.bottomText}
+                        onChange={handleChange}
+                    />
+               
+                <button className='form-button' onClick={getMemeImage} >Get a new meme image </button>
+            </div>
+            <div className='meme'>
+                <img src={meme.randomImage} />
+                <span className='top'>{meme.topText}</span>
+                <span className='bottom'>{meme.bottomText}</span>
+
+            </div>
+        </main>
+    )
+}
+
+export default Meme
